aa953e7fc63f83de457454800885eb538ffd3259
itsmanish0001 <96372438+itsmanish0001@users.noreply.github.com>
2024-11-03 14:45:16 +0530

    Create infix_to_prefix.cpp

---
+++ infix_to_prefix.cpp
@@ -0,0 +1,79 @@
+ int priority(char ch){
+        if(ch == '('){
+            return 0;
+        }
+        else if(ch == '+' || ch == '-'){
+            return 1;
+        }
+        else if(ch == '*' || ch == '/'){
+            return 2;
+        }
+        else if(ch == '^'){
+            return 3;
+        }
+       
+    }
+    
+    string infixToPrefix(string str) {
+        
+       string ans = "";
+       stack<char> s;
+       reverse(str.begin(), str.end());
+       
+       for(int i = 0; i < str.size(); i++){
+           if(str[i] == '('){
+               str[i] = ')';
+           }
+           
+           else if(str[i] == ')'){
+               str[i] = '(';
+           }
+       }
+       
+       for(auto ch: str){
+           if(isalpha(ch) || isdigit(ch)){
+               ans.push_back(ch);
+           }
+           else if(ch == '('){
+               s.push(ch);
+           }
+           else if(ch == ')'){
+               while(s.top() != '('){
+                   ans.push_back(s.top());
+                   s.pop();
+               }
+               s.pop();
+           }
+           else{
+               if(ch == '^'){
+                   while(s.size() != 0 && priority(ch) <= priority(s.top())){
+                   ans.push_back(s.top());
+                   s.pop();
+               }
+               s.push(ch);
+               }
+               else{
+                   while(s.size() != 0 && priority(ch) < priority(s.top())){
+                   ans.push_back(s.top());
+                   s.pop();
+               }
+               s.push(ch);
+               }
+           }
+       }
+       
+       
+       while(s.size() != 0){
+           ans.push_back(s.top());
+           s.pop();
+       }
+       
+       
+       reverse(ans.begin(), ans.end());
+       
+       return ans;
+       
+       
+       
+       
+    }
